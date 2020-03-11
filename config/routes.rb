5a6f36eb4f588407b3810389391fe1b9f5e1bcef
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-03-11 17:44:34 +0900

    devise_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show, :new, :edit, :update] do
-    collection do
-      get :login
-      get :logout
-    end
-  end
+  
   root "tops#index"
 
 
