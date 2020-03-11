1a7ace69075ccdd9e2a76f30cc1225297185e7f8
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-03-11 14:51:58 +0900

    途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :new, :edit, :update] do
+    collection do
+      get :login
+      get :logout
+    end
+  end
   root "tops#index"
+
 
 end
