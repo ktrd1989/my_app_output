8274619874c9977356e8db68a5e804498fb74f3a
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-03-13 01:33:41 +0900

    検索機能

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,11 @@ class Report < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :comments
+
+  #reportテーブルから検索するというメソッドのため、modelに記述
+  def self.search(search)
+    #引数で渡されるsearchの中身に何もなければ全ての投稿を取得できる
+    return Report.all unless search
+    Report.where('title LIKE(?)', "%#{search}%")
+  end
 end
