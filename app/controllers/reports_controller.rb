83ebff63ab53c1cf9a76a57dc4919e6c62a0329c
ktrd1989 <59087649+ktrd1989@users.noreply.github.com>
2020-03-12 13:11:43 +0900

    投稿（途中）

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,15 @@
 class ReportsController < ApplicationController
+  def new
+    @report = Report.new
+  end
+
+  def create
+    Report.create(report_params)
+    redirect_to root_path
+  end
+
+  private
+    def  report_params
+      params.require(:report).permit(:title, :review, :image)
+    end
 end
